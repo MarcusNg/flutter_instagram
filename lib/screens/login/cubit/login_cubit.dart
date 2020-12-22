93e55d0704b444cf00a94b883437044733d0dfc2
Marcus Ng <marcuslistanng@gmail.com>
2020-12-22 06:34:27 -0500

    add login cubit

---
+++ lib/screens/login/cubit/login_cubit.dart
@@ -0,0 +1,37 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_instagram/models/models.dart';
+import 'package:flutter_instagram/repositories/repositories.dart';
+import 'package:meta/meta.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  final AuthRepository _authRepository;
+
+  LoginCubit({@required AuthRepository authRepository})
+      : _authRepository = authRepository,
+        super(LoginState.initial());
+
+  void emailChanged(String value) {
+    emit(state.copyWith(email: value, status: LoginStatus.initial));
+  }
+
+  void passwordChanged(String value) {
+    emit(state.copyWith(password: value, status: LoginStatus.initial));
+  }
+
+  void logInWithCredentials() async {
+    if (state.isFormValid || state.status == LoginStatus.submitting) return;
+    emit(state.copyWith(status: LoginStatus.submitting));
+    try {
+      await _authRepository.logInWithEmailAndPassword(
+        email: state.email,
+        password: state.password,
+      );
+      emit(state.copyWith(status: LoginStatus.success));
+    } on Failure catch (err) {
+      emit(state.copyWith(failure: err, status: LoginStatus.error));
+    }
+  }
+}
