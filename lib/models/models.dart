1aa5d232a892ed9ccd921dac4118df43548cb56e
Marcus Ng <marcuslistanng@gmail.com>
2021-01-15 21:06:27 -0500

    add user model

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,1 +1,2 @@
 export 'failure_model.dart';
+export 'user_model.dart';
---
+++ lib/models/user_model.dart
@@ -0,0 +1,89 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+class User extends Equatable {
+  final String id;
+  final String username;
+  final String email;
+  final String profileImageUrl;
+  final int followers;
+  final int following;
+  final String bio;
+
+  const User({
+    @required this.id,
+    @required this.username,
+    @required this.email,
+    @required this.profileImageUrl,
+    @required this.followers,
+    @required this.following,
+    @required this.bio,
+  });
+
+  static const empty = User(
+    id: '',
+    username: '',
+    email: '',
+    profileImageUrl: '',
+    followers: 0,
+    following: 0,
+    bio: '',
+  );
+
+  @override
+  List<Object> get props => [
+        id,
+        username,
+        email,
+        profileImageUrl,
+        followers,
+        following,
+        bio,
+      ];
+
+  User copyWith({
+    String id,
+    String username,
+    String email,
+    String profileImageUrl,
+    int followers,
+    int following,
+    String bio,
+  }) {
+    return User(
+      id: id ?? this.id,
+      username: username ?? this.username,
+      email: email ?? this.email,
+      profileImageUrl: profileImageUrl ?? this.profileImageUrl,
+      followers: followers ?? this.followers,
+      following: following ?? this.following,
+      bio: bio ?? this.bio,
+    );
+  }
+
+  Map<String, dynamic> toDocument() {
+    return {
+      'username': username,
+      'email': email,
+      'profileImageUrl': profileImageUrl,
+      'followers': followers,
+      'following': following,
+      'bio': bio,
+    };
+  }
+
+  factory User.fromDocument(DocumentSnapshot doc) {
+    if (doc == null) return null;
+    final data = doc.data();
+    return User(
+      id: doc.id,
+      username: data['username'] ?? '',
+      email: data['email'] ?? '',
+      profileImageUrl: data['profileImageUrl'] ?? '',
+      followers: (data['followers'] ?? 0).toInt(),
+      following: (data['following'] ?? 0).toInt(),
+      bio: data['bio'] ?? '',
+    );
+  }
+}
