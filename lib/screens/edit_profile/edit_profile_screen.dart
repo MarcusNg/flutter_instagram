862336825e7d28f4e4a1ba86eb952c0ef16ec40a
Marcus Ng <marcuslistanng@gmail.com>
2021-02-01 16:40:39 -0500

    add edit profile screen

--- lib/screens/edit_profile/edit_profile_screen.dart
+++ lib/screens/edit_profile/edit_profile_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'package:flutter_instagram/models/models.dart';
 import 'package:flutter_instagram/repositories/repositories.dart';
 import 'package:flutter_instagram/screens/edit_profile/cubit/edit_profile_cubit.dart';
 import 'package:flutter_instagram/screens/profile/bloc/profile_bloc.dart';
+import 'package:flutter_instagram/widgets/widgets.dart';
 
 class EditProfileScreenArgs {
   final BuildContext context;
@@ -23,17 +25,115 @@ class EditProfileScreen extends StatelessWidget {
           storageRepository: context.read<StorageRepository>(),
           profileBloc: args.context.read<ProfileBloc>(),
         ),
-        child: EditProfileScreen(),
+        child: EditProfileScreen(
+            user: args.context.read<ProfileBloc>().state.user),
       ),
     );
   }
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final User user;
+
+  EditProfileScreen({
+    Key key,
+    @required this.user,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Edit Profile'),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Edit Profile'),
+        ),
+        body: BlocConsumer<EditProfileCubit, EditProfileState>(
+          listener: (context, state) {
+            if (state.status == EditProfileStatus.success) {
+              Navigator.of(context).pop();
+            } else if (state.status == EditProfileStatus.error) {
+              showDialog(
+                context: context,
+                builder: (context) =>
+                    ErrorDialog(content: state.failure.message),
+              );
+            }
+          },
+          builder: (context, state) {
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  if (state.status == EditProfileStatus.submitting)
+                    const LinearProgressIndicator(),
+                  const SizedBox(height: 32.0),
+                  GestureDetector(
+                    onTap: () => _selectProfileImage(context),
+                    child: UserProfileImage(
+                      radius: 80.0,
+                      profileImageUrl: user.profileImageUrl,
+                      profileImage: state.profileImage,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          TextFormField(
+                            initialValue: user.username,
+                            decoration: InputDecoration(hintText: 'Username'),
+                            onChanged: (value) => context
+                                .read<EditProfileCubit>()
+                                .usernameChanged(value),
+                            validator: (value) => value.trim().isEmpty
+                                ? 'Username cannot be empty.'
+                                : null,
+                          ),
+                          const SizedBox(height: 16.0),
+                          TextFormField(
+                            initialValue: user.bio,
+                            decoration: InputDecoration(hintText: 'Bio'),
+                            onChanged: (value) => context
+                                .read<EditProfileCubit>()
+                                .bioChanged(value),
+                            validator: (value) => value.trim().isEmpty
+                                ? 'Bio cannot be empty.'
+                                : null,
+                          ),
+                          const SizedBox(height: 28.0),
+                          RaisedButton(
+                            elevation: 1.0,
+                            color: Theme.of(context).primaryColor,
+                            textColor: Colors.white,
+                            onPressed: () => _submitForm(
+                              context,
+                              state.status == EditProfileStatus.submitting,
+                            ),
+                            child: const Text('Update'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
+  }
+
+  void _selectProfileImage(BuildContext context) {
+    // TODO: implement
+  }
+
+  void _submitForm(BuildContext context, bool isSubmitting) {
+    if (_formKey.currentState.validate() && !isSubmitting) {
+      context.read<EditProfileCubit>().submit();
+    }
   }
 }
