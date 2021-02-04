776d871f661f1f3d6a550dd3f3dede0bbf3bf70e
Marcus Ng <marcuslistanng@gmail.com>
2021-02-04 15:38:01 -0500

    add comments bloc

---
+++ lib/screens/comments/comments_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CommentsScreen extends StatefulWidget {
+  @override
+  _CommentsScreenState createState() => _CommentsScreenState();
+}
+
+class _CommentsScreenState extends State<CommentsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
